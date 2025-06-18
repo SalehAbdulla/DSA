67904b3c82e96a17c31a7bb04859bdf5a6829d86
Saleh Abdulla <salehabdullaict@gmail.com>
2025-06-18 14:23:47 +0300

    Solved using SubString

--- LongestSubString/LongestSubString/src/Main.java
+++ LongestSubString/LongestSubString/src/Main.java
@@ -11,15 +11,12 @@ public class Main {
         for (int rightSlide = 0; rightSlide < s.length(); rightSlide++) {
             char currentChar = s.charAt(rightSlide);
 
-            // Check if current character exists in the window
             int index = s.substring(leftSlide, rightSlide).indexOf(currentChar);
 
             if (index != -1) {
-                // Move leftSlide just after the repeated character
                 leftSlide = leftSlide + index + 1;
             }
 
-            // Update the max length
             max = Math.max(max, rightSlide - leftSlide + 1);
         }
 
